5b33783830d6acf05b9e4ca3aac3926f09a86cbe
Megan Hepner <megan.hepner@gmail.com>
2020-06-16 13:53:41 -0700

    add js to movie ticket to produce ticket price
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>;

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 //Business Logic
 
+function Ticket(movie, time, age, discounts){
+  this.movie = movie,
+  this.time = time,
+  this.age = age,
+  this.discounts = discounts
+}
+
+Ticket.prototype.ticketPrice = function(ticketPrice, discountPrice){
+  let outputPrice = 0;
+  outputPrice = ticketPrice - (discountPrice * this.discounts);
+  return outputPrice;
+}
 
 
 
@@ -7,6 +19,23 @@
 //UI Logic
 $(document).ready(function() {
   $("form#userInput").submit(function(event){
+    event.preventDefault();
+    const inputMovie = $("select#movie option:selected").text();
+    const inputTime = $("select#time option:selected").text();
+    const inputAge = $("select#age option:selected").text();
+    let discount =0;
+    $("select").each(function(){
+      discount += parseInt($(this).val());
+    })
+    const inputDiscounts = discount;
+    let ticket = new Ticket(inputMovie, inputTime, inputAge, inputDiscounts);
+    const outputPrice = ticket.ticketPrice(16, 2);
+    $(".movie").text(ticket.movie);
+    $(".time").text(ticket.time);
+    $(".age").text(ticket.age);
+    $(".price").text(outputPrice);
+    $("#show-input").show();
 
   });
 });
+
